3a1859c64edefb6c07b8c9387986a0b8047db61c
afearing <afearing@berkeley.edu>
2021-01-28 13:04:40 -0800

    deleted starting code

---
+++ aruco_tags/src/yoga_planning/src/tag_broadcaster.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from fiducial_msgs.msg import FiducialTransformArray
+from fiducial_msgs.msg import FiducialTransform
+import rospy
+import tf
+
+
+# Callback function to subscribe to images
+def fiducial_callback(fiducial_tfArray):
+    vector3 = fiducial_tfArray.transforms[0].transform.translation
+    quaternion = fiducial_tfArray.transforms[0].transform.rotation
+
+    br = tf.TransformBroadcaster()
+    rate = rospy.Rate(10.0)
+    while not rospy.is_shutdown():
+        br.sendTransform((vector3.x, vector3.y, vector3.z),
+                            (quaternion.x, quaternion.y, quaternion.z, quaternion.w),
+                            rospy.Time.now(),
+                            "aruco_tag",
+                            "head_camera")
+        rate.sleep()
+        
+if __name__ == '__main__':
+    rospy.init_node('aruco_broadcaster',anonymous=True) # Initialze ROS node
+    rospy.Subscriber('/fiducial_transforms', FiducialTransformArray, fiducial_callback)
+    rospy.spin() 
--- aruco_tags/src/yoga_planning/src/yog.py
+++ aruco_tags/src/yoga_planning/src/yog.py
@@ -1,26 +1,70 @@
-#!/usr/bin/env python
-from fiducial_msgs.msg import FiducialTransformArray
-from fiducial_msgs.msg import FiducialTransform
+import sys
+import copy
 import rospy
-import tf
+importmoveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+import numpy as np
+import std_msgs.msg
 
 
-# Callback function to subscribe to images
-def fiducial_callback(fiducial_tfArray):
-    vector3 = fiducial_tfArray.transforms[0].transform.translation
-    quaternion = fiducial_tfArray.transforms[0].transform.rotation
 
-    br = tf.TransformBroadcaster()
-    rate = rospy.Rate(10.0)
-    while not rospy.is_shutdown():
-        br.sendTransform((vector3.x, vector3.y, vector3.z),
-                            (quaternion.x, quaternion.y, quaternion.z, quaternion.w),
-                            rospy.Time.now(),
-                            "aruco_tag",
-                            "head_camera")
-        rate.sleep()
-        
+def fiducicial_callback(fiducial_tfArray):
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node('yog', anonymous=true)
+    robot = moveit_commander.RobotCommander()
+    scene = moveit_commander.PlanningSCeneInterface()
+
+
+
 if __name__ == '__main__':
-    rospy.init_node('aruco_broadcaster',anonymous=True) # Initialze ROS node
     rospy.Subscriber('/fiducial_transforms', FiducialTransformArray, fiducial_callback)
-    rospy.spin() 
+
+
+
+class YogaPlanner(object):
+    def __init__(self, group_name):
+        """
+        Constructor.
+
+        Inputs:
+        group_name: the name of the move_group.
+            For Baxter, this would be 'left_arm' or 'right_arm'
+            For Sawyer, this would be 'right_arm'
+        """
+
+        # If the node is shutdown, call this function    
+        rospy.on_shutdown(self.shutdown)
+
+        # Initialize moveit_commander
+        moveit_commander.roscpp_initialize(sys.argv)
+
+        # Initialize the robot
+        self._robot = moveit_commander.RobotCommander()
+
+        # Initialize the planning scene
+        self._scene = moveit_commander.PlanningSceneInterface()
+
+        # This publishes updates to the planning scene
+        self._planning_scene_publisher = rospy.Publisher('/collision_object', CollisionObject, queue_size=10)
+
+        # Instantiate a move group
+        self._group = moveit_commander.MoveGroupCommander(group_name)
+
+        # Set the maximum time MoveIt will try to plan before giving up
+        self._group.set_planning_time(5)
+
+        # Set the bounds of the workspace
+        self._group.set_workspace([-2, -2, -2, 2, 2, 2])
+
+        # Sleep for a bit to ensure that all inititialization has finished
+        rospy.sleep(0.5)
+
+    def shutdown(self):
+        """
+        Code to run on shutdown. This is good practice for safety
+
+        Currently deletes the object's MoveGroup, so that further commands will do nothing
+        """
+        self._group = None
+        rospy.loginfo("Stopping Path Planner")
